c362c8c01317d8837d3f0d5712f8e9bb85c7bf07
dimah <dimah.code@gmail.com>
2026-01-05 06:56:34 +0330

    refactor: refactor views.py

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from .views import snippet_detail, snippet_list
 
 urlpatterns = [
     path("snippets/", snippet_list),
     path("snippets/<int:pk>/", snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
