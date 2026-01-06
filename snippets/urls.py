1c0267ee95104474a0609eabc81002ac91cf450f
dimah <dimah.code@gmail.com>
2026-01-06 10:22:26 +0330

    feat: add user urls to snippets/urls.py

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
+
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import SnippetDetail, SnippetList
+from .views import (
+    SnippetDetail,
+    SnippetList,
+    UserDetail,
+    UserList,
+)
 
 urlpatterns = [
+    # Snippets
     path("snippets/", SnippetList.as_view()),
     path("snippets/<int:pk>/", SnippetDetail.as_view()),
+    # Users
+    path("users/", UserList.as_view()),
+    path("user/<int:pk>/", UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
