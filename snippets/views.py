e84bc367f68390c66ca14bc3b4fa76eefbbe9506
dimah <dimah.code@gmail.com>
2026-01-06 06:06:26 +0330

    refactor: refactor snippet/views.py with generics views : class-based

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,10 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-class SnippetList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView,
-):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets and create a new snippet
     """
@@ -17,47 +12,11 @@ class SnippetList(
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        """
-        List all code snippets
-        """
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        """
-        Post a code snippet with valid data
-        """
-
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a code snippet
     """
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        """
-        Return an specific snippet
-        """
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        """
-        Update an specific snippet with given valid data
-        """
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        """
-        Delete an specific Snippet with pk id
-        """
-        return self.destroy(request, *args, **kwargs)
