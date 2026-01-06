9904c6532c59fc9300c935fa81c3ba0d4168744d
dimah <dimah.code@gmail.com>
2026-01-07 01:20:34 +0330

    feat: add IsAuthorOrReadOnly permission to snippets/views.py

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
@@ -13,6 +14,7 @@ class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -25,6 +27,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class UserList(generics.ListAPIView):
