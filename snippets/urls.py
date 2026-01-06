579186378705a6e77c282202369f477062bdcf95
dimah <dimah.code@gmail.com>
2026-01-07 02:46:02 +0330

    feat: add root endpoint and named all endpoints

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,17 +5,26 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import (
     SnippetDetail,
     SnippetList,
+    SnippetHighlight,
     UserDetail,
     UserList,
+    api_root,
 )
 
-urlpatterns = [
-    # Snippets
-    path("snippets/", SnippetList.as_view()),
-    path("snippets/<int:pk>/", SnippetDetail.as_view()),
-    # Users
-    path("users/", UserList.as_view()),
-    path("user/<int:pk>/", UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(
+    [
+        # Root
+        path("", api_root),
+        # Snippets
+        path("snippets/", SnippetList.as_view(), name="snippet-list"),
+        path("snippets/<int:pk>/", SnippetDetail.as_view(), name="snippet-detail"),
+        path(
+            "snippets/<int:pk>/highlight/",
+            SnippetHighlight.as_view(),
+            name="snippet-highlight",
+        ),
+        # Users
+        path("users/", UserList.as_view(), name="user-list"),
+        path("user/<int:pk>/", UserDetail.as_view(), name="user-detail"),
+    ]
+)
